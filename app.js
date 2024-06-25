803e7c1b2a65570ee1bccd87ccba266fdd86d665
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-06-25 13:54:02 +0300

    Updated the Desktop view

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const url = `https://api.adviceslip.com/advice`;
-const adviceNumber = document.getElementById('advice-number');
-const adviceBody = document.getElementById('advice');
+document.addEventListener('DOMContentLoaded',()=>{
+    
+    document.getElementById('advice-button').addEventListener('click', getAdvice)
 
-async function getAdvice(){
-    try{
-        const response = await axios.get(url);
-        const advice = response.data.slip;
-        adviceNumber.innerText = advice.id;
-        adviceBody.innerText = advice.advice;
-        
-    } catch(err){
-        console.log(err)
-    }
-}
+    async function getAdvice(){
+        //Added timestamp query to bypass cache on the api allowing unique request on each click
+        const url = `https://api.adviceslip.com/advice?timestamp=${new Date().getTime()}`;
+        const adviceNumber = document.getElementById('advice-number');
+        const adviceBody = document.getElementById('advice');
 
-getAdvice();
+        try{
+            const response = await axios.get(url);
+            const advice = response.data.slip;
+            adviceNumber.innerText = advice.id;
+            adviceBody.innerText = advice.advice;            
+        } catch(err){
+            console.log(err)
+        }
+    } 
+    getAdvice()
+})
